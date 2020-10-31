e21fe8e7ed828ffc0b7818cf162bbbadbe683f4b
Arafa islam <ajjhalak@gmail.com>
2020-11-01 02:26:01 +0600

    Updates

---
+++ precache-manifest.741f4aedec3a60f691959a71eef3331a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d53955298949526843d45570a1d0dc",
+    "url": "/reactweb/index.html"
+  },
+  {
+    "revision": "852a364adf78091a18a2",
+    "url": "/reactweb/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "9c85a9096ade7e3b8df5",
+    "url": "/reactweb/static/css/main.b223e354.chunk.css"
+  },
+  {
+    "revision": "852a364adf78091a18a2",
+    "url": "/reactweb/static/js/2.c02f176b.chunk.js"
+  },
+  {
+    "revision": "d877b083b942576a955038ed13c1c8e7",
+    "url": "/reactweb/static/js/2.c02f176b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c85a9096ade7e3b8df5",
+    "url": "/reactweb/static/js/main.f5d78877.chunk.js"
+  },
+  {
+    "revision": "2af806816c834ba8cb63",
+    "url": "/reactweb/static/js/runtime-main.43e6d697.js"
+  },
+  {
+    "revision": "5b3a40db6d99c9c878f7964f49193bf4",
+    "url": "/reactweb/static/media/01.5b3a40db.svg"
+  },
+  {
+    "revision": "8cb396b9c1efba2b2ffd12ad2a126051",
+    "url": "/reactweb/static/media/02.8cb396b9.svg"
+  }
+]);
